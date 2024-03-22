a8db4a6d4cc2194cb70ae51485509372390bc4cd
Juan José Ordoñez Villalobos <49448784+coltonsini@users.noreply.github.com>
2024-03-22 07:17:06 -0500

    CrudCreation

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://estjuanjordonez:2gH3DtvCEDQJDyPN@gymdatabase.upwmysx.mongodb.net/', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('No se pudo conectar a MongoDB', err));
+
+// Definir el esquema y modelo de usuario
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware para parsear JSON en las solicitudes POST
+app.use(express.json());
+
+// Rutas
+app.get('/api/models', async (req, res) => {
+    try {
+        const models = await User.find();
+        res.send(models);
+    } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+app.post('/api/models', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+        const savedUser = await newUser.save();
+        res.status(201).send(savedUser);
+    } catch (error) {
+        console.error('Error al guardar el usuario:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Node.js corriendo en el puerto ${PORT}`);
+});
